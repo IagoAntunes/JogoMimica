b4302958e6434f214aa0abc77a3c364f9c139c16
iagoa <iagoantunes.f@gmail.com>
2022-01-12 10:53:48 -0300

    MVVM - Tela Jogo

--- JogoMimica/JogoMimica/ViewModel/JogoViewModel.cs
+++ JogoMimica/JogoMimica/ViewModel/JogoViewModel.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace JogoMimica.ViewModel
 {
     public class JogoViewModel
     {
+        public string Palavra { get; set; }
+        public short PalavraPontuacao { get; set; }
+        public short TextoContagem { get; set; }
+        public Boolean ContainerContagem { get; set; }
+        public Boolean ContainerIniciar { get; set; }
+
+
+        public Command MostrarPalavra { get; set; }
+        public Command Acertou { get; set; }
+        public Command Errou { get; set; }
+        public Command Iniciar { get; set; }
+        
+
+        public JogoViewModel()
+        {
+            ContainerContagem = false;
+            MostrarPalavra = new Command();
+            Acertou = new Command();
+            Errou = new Command();
+            Iniciar = new Command();
+        }
     }
 }
